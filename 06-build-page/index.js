67d0e326962d8f5039a8027dc47d6e0fe72d31d7
AliaksandrButsin <aliaksandrbutsin@gmail.com>
2023-05-08 13:12:30 +0200

    add: async

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -16,47 +16,48 @@ const articlesPath = path.join(componentsPath, 'articles.html');
 const footerPath = path.join(componentsPath, 'footer.html');
 const indexPath = path.join(projectDistPath, 'index.html');
 
-let indexHtml = fs.readFileSync(templatePath, 'utf8');
-indexHtml = indexHtml.replace('{{header}}', fs.readFileSync(headerPath, 'utf8'));
-indexHtml = indexHtml.replace('{{articles}}', fs.readFileSync(articlesPath, 'utf8'));
-indexHtml = indexHtml.replace('{{footer}}', fs.readFileSync(footerPath, 'utf8'));
-fs.writeFileSync(indexPath, indexHtml);
+(async () => {
+  let indexHtml = await fs.promises.readFile(templatePath, 'utf8');
+  indexHtml = indexHtml.replace('{{header}}', await fs.promises.readFile(headerPath, 'utf8'));
+  indexHtml = indexHtml.replace('{{articles}}', await fs.promises.readFile(articlesPath, 'utf8'));
+  indexHtml = indexHtml.replace('{{footer}}', await fs.promises.readFile(footerPath, 'utf8'));
+  await fs.promises.writeFile(indexPath, indexHtml);
 
-// Собираем стили в style.css
-const stylesPath = path.join(__dirname, 'styles');
-const styleFilePath = path.join(projectDistPath, 'style.css');
+  // Собираем стили в style.css
+  const stylesPath = path.join(__dirname, 'styles');
+  const styleFilePath = path.join(projectDistPath, 'style.css');
 
-const files = fs.readdirSync(stylesPath);
-let styles = '';
-files.forEach((file) => {
-  const filePath = path.join(stylesPath, file);
-  const stat = fs.statSync(filePath);
-  if (stat.isFile()) {
-    styles += fs.readFileSync(filePath, 'utf8');
-  }
-});
-fs.writeFileSync(styleFilePath, styles);
-
-// Копируем папку assets
-const assetsPath = path.join(__dirname, 'assets');
-const assetsDistPath = path.join(projectDistPath, 'assets');
-
-const copyFile = promisify(fs.copyFile);
-const mkdir = promisify(fs.mkdir);
-const copyDir = async (src, dest) => {
-  await mkdir(dest, { recursive: true });
-  const entries = await fs.promises.readdir(src, { withFileTypes: true });
-  for (let entry of entries) {
-    const srcPath = path.join(src, entry.name);
-    const destPath = path.join(dest, entry.name);
-    if (entry.isDirectory()) {
-      await copyDir(srcPath, destPath);
-    } else {
-      await copyFile(srcPath, destPath);
+  const files = fs.readdirSync(stylesPath);
+  let styles = '';
+  for (let file of files) {
+    const filePath = path.join(stylesPath, file);
+    const stat = fs.statSync(filePath);
+    if (stat.isFile()) {
+      styles += await fs.promises.readFile(filePath, 'utf8');
     }
   }
-};
+  await fs.promises.writeFile(styleFilePath, styles);
 
-copyDir(assetsPath, assetsDistPath)
-  .then(() => console.log('Build successful!'))
-  .catch((err) => console.error(err));
+  // Копируем папку assets
+  const assetsPath = path.join(__dirname, 'assets');
+  const assetsDistPath = path.join(projectDistPath, 'assets');
+
+  const copyFile = promisify(fs.copyFile);
+  const mkdir = promisify(fs.mkdir);
+  const copyDir = async (src, dest) => {
+    await mkdir(dest, { recursive: true });
+    const entries = await fs.promises.readdir(src, { withFileTypes: true });
+    for (let entry of entries) {
+      const srcPath = path.join(src, entry.name);
+      const destPath = path.join(dest, entry.name);
+      if (entry.isDirectory()) {
+        await copyDir(srcPath, destPath);
+      } else {
+        await copyFile(srcPath, destPath);
+      }
+    }
+  };
+
+  await copyDir(assetsPath, assetsDistPath);
+  console.log('Все задачи выполненны успешно!');
+})();
